9fda10670cd0021e0981b5b5dc64d21cb103657c
Sergey Perepelitsa <non_2001@mail.ru>
2018-08-07 22:34:01 +0300

    file filter improvements version adjusted

--- SpriteBuilder/ccBuilder/AppDelegate.m
+++ SpriteBuilder/ccBuilder/AppDelegate.m
@@ -2792,6 +2792,10 @@ typedef void (^SetNodeParamBlock)(CCNode*, id);
 
 -(void)setFilterString:(NSString*)filterString
 {
+    if(filterTimer) {
+        [filterTimer invalidate];
+        filterTimer = nil;
+    }
     _filterString = filterString;
     projectOutlineHandler.filter = filterString;
     [outlineProject reloadData];
@@ -2799,9 +2803,23 @@ typedef void (^SetNodeParamBlock)(CCNode*, id);
         [outlineProject expandItem:nil expandChildren:YES];
 }
 
+-(void)setFilterStringByTimer:(NSTimer*)timer
+{
+    [self setFilterString:timer.userInfo];
+}
+
 - (void)controlTextDidChange:(NSNotification *)notification {
     NSTextField *textField = [notification object];
-    self.filterString = textField.stringValue;
+    if(filterTimer)
+    {
+        [filterTimer invalidate];
+    }
+    filterTimer = [NSTimer scheduledTimerWithTimeInterval:1
+                                     target:self
+                                   selector:@selector(setFilterStringByTimer:)
+                                   userInfo:textField.stringValue
+                                    repeats:YES];
+    
 }
 
 
